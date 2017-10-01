0ae023348269c41cc16d6685d401ac88a160dffc
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-10-01 17:50:53 -0500

    initial site files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<95o4Rtl93UNSQlJlA3pNk9iHu>',
+  consumer_secret: '<QhlnqhMNMeoLNnCwuqyYGv4Pti3qsVIR35gpaxxgIQd8VY18nL>',
+  access_token_key: '<914592319680245762-6a3WGbUhLJx3lAUAdWOx32PgQ8o8u8b>',
+  access_token_secret: '<914592319680245762-6a3WGbUhLJx3lAUAdWOx32PgQ8o8u8b>',
+}
+
+module.exports = twitterKeys;
