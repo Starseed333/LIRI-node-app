705ee894bfbf52af3fa62209b7be6085d85ffe4e
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-10-06 20:00:26 -0500

    updates

--- keys.js
+++ keys.js
@@ -4,7 +4,7 @@ var twitterKeys = {
   consumer_key: '<95o4Rtl93UNSQlJlA3pNk9iHu>',
   consumer_secret: '<QhlnqhMNMeoLNnCwuqyYGv4Pti3qsVIR35gpaxxgIQd8VY18nL>',
   access_token_key: '<914592319680245762-6a3WGbUhLJx3lAUAdWOx32PgQ8o8u8b>',
-  access_token_secret: '<914592319680245762-6a3WGbUhLJx3lAUAdWOx32PgQ8o8u8b>',
+  access_token_secret: '<BuxofsbIVOsYxs9ZhTaAD0t7oPiG2JXvGiJJAqrmDsUOV>',
 }
 
 module.exports = twitterKeys;
