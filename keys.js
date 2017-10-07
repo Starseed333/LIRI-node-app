1fe76465865b3fc26e657de8df0e0522afa4b197
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-10-06 20:26:56 -0500

    complete

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<95o4Rtl93UNSQlJlA3pNk9iHu>',
-  consumer_secret: '<QhlnqhMNMeoLNnCwuqyYGv4Pti3qsVIR35gpaxxgIQd8VY18nL>',
-  access_token_key: '<914592319680245762-6a3WGbUhLJx3lAUAdWOx32PgQ8o8u8b>',
-  access_token_secret: '<BuxofsbIVOsYxs9ZhTaAD0t7oPiG2JXvGiJJAqrmDsUOV>',
+  consumer_key: '95o4Rtl93UNSQlJlA3pNk9iHu',
+  consumer_secret: 'QhlnqhMNMeoLNnCwuqyYGv4Pti3qsVIR35gpaxxgIQd8VY18nL',
+  access_token_key: '914592319680245762-0ieWrgxPogzFCLCemejLRnjcfRxgujG',
+  access_token_secret: 'JoBj5Blu2YBZRPhEQmLqsWfPUXI995nkVLm0hyvkJm2Kl',
 }
 
 module.exports = twitterKeys;
